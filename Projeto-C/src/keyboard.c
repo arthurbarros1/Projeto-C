03e6447d58fbbc11764c8c37e1744e8b45aad385
pedrocmmaia <121133422+pedrocmmaia@users.noreply.github.com>
2024-11-01 19:35:57 -0300

    Update keyboard.c

--- Projeto-C/src/keyboard.c
+++ Projeto-C/src/keyboard.c
@@ -1,68 +1,36 @@
-/**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
+#include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <fcntl.h>
 #include "keyboard.h"
 
-static struct termios initialSettings, newSettings;
-static int peekCharacter;
+static struct termios oldt, newt;
 
-
-void keyboardInit()
-{
-    tcgetattr(0,&initialSettings);
-    newSettings = initialSettings;
-    newSettings.c_lflag &= ~ICANON;
-    newSettings.c_lflag &= ~ECHO;
-    newSettings.c_lflag &= ~ISIG;
-    newSettings.c_cc[VMIN] = 1;
-    newSettings.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSANOW, &newSettings);
+// Configura o terminal para o modo sem buffer
+void keyboardInit() {
+    tcgetattr(STDIN_FILENO, &oldt); // Salva o estado atual do terminal
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO); // Desabilita o modo canônico e o eco
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Aplica as configurações
 }
 
-void keyboardDestroy()
-{
-    tcsetattr(0, TCSANOW, &initialSettings);
+// Verifica se uma tecla foi pressionada
+int keyhit() {
+    struct timeval tv = { 0L, 0L };
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
 }
 
-int keyhit()
-{
-    unsigned char ch;
-    int nread;
-
-    if (peekCharacter != -1) return 1;
-    
-    newSettings.c_cc[VMIN]=0;
-    tcsetattr(0, TCSANOW, &newSettings);
-    nread = read(0,&ch,1);
-    newSettings.c_cc[VMIN]=1;
-    tcsetattr(0, TCSANOW, &newSettings);
-    
-    if(nread == 1) 
-    {
-        peekCharacter = ch;
-        return 1;
-    }
-    
-    return 0;
-}
-
-int readch()
-{
-    char ch;
-
-    if(peekCharacter != -1)
-    {
-        ch = peekCharacter;
-        peekCharacter = -1;
-        return ch;
-    }
-    read(0,&ch,1);
+// Lê a tecla pressionada
+int readch() {
+    int ch;
+    ch = getchar();
     return ch;
+}
+
+// Restaura as configurações originais do terminal
+void keyboardDestroy() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
